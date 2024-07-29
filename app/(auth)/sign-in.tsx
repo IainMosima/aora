f4bf3f7cc319dbb09fa8518faad70cb1aefe7e83
Iain Mosima <iain.nyakundi@apollo.ac.ke>
2024-07-29 21:00:32 +0300

    finished writing layouts for sign-up and sign-in

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -4,6 +4,8 @@ import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '@/components/FormField'
+import CustomButton from '@/components/CustomButton'
+import { Link } from 'expo-router'
 
 export interface SignInForm {
   email: string,
@@ -15,11 +17,15 @@ const SignIn = () => {
     email: '',
     password: '',
   })
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const submit = () => {
+
+  }
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaView className='bg-primary h-full'>
         <ScrollView>
-          <View className='w-full justify-center h-full px-4 my-6'>
+          <View className='w-full justify-center min-h-[75vh] px-4 my-6'>
             <Image source={images.logo} className='w-[115px] h-[35px]' />
             <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>Log in to Aora</Text>
             <FormField
@@ -36,6 +42,18 @@ const SignIn = () => {
               handleChangeText={e => setForm({ ...form, password: e })}
               otherStyle="mt-7"
             />
+
+            <CustomButton
+              title='Sign In'
+              handlePress={submit}
+              containerStyles='mt-7'
+              isLoading={isSubmitting}
+            />
+
+            <View className='justify-center pt-5 flex-row gap-2'>
+              <Text className='text-lg text-gray-100 font-pregular'>Don't have an account?</Text>
+              <Link href="/sign-up" className='text-lg text-secondary'>Sign Up</Link>
+            </View>
           </View>
         </ScrollView>
       </SafeAreaView>
--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,7 +1,7 @@
-import { View, Text } from 'react-native'
+import { icons } from '@/constants'
 import React, { useState } from 'react'
-import { SignInForm } from '@/app/(auth)/sign-in'
-import { TextInput } from 'react-native-gesture-handler'
+import { Image, Text, TouchableOpacity, View, TextInput } from 'react-native'
+
 
 interface Props {
     title: string,
@@ -18,15 +18,20 @@ const FormField = (props: Props) => {
         <View className={`space-y-2 ${props.otherStyle}`}>
             <Text className='text-base text-gray-100 font-pmedium'>{props.title}</Text>
 
-            <View className='border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center'>
+            <View className='border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary flex flex-row items-center'>
                 <TextInput
                     className='flex-1 text-white font-psemibold text-base'
                     value={props.value}
                     placeholder={props.placeholder}
                     placeholderTextColor='#7b7b8b'
                     onChangeText={props.handleChangeText}
-                    secureTextEntry={props.title === 'password' && !showPassword}
+                    secureTextEntry={props.title === 'Password' && !showPassword}
                 />
+                {props.title === 'Password' && (
+                    <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                        <Image source={!showPassword ? icons.eye : icons.eyeHide } className='w-6 h-6' resizeMode='contain'/>
+                    </TouchableOpacity>
+                )}
             </View>
         </View>
     )
